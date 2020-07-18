0a9286653d4f3d7f401f4401250c08a59e505eb6
Haylee Ham <hayleeham@gmail.com>
2020-07-18 13:24:43 -0400

    Finished 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+ratio = 3/5
+bounce_number = 0
+
+while bounce_number < 10:
+    # keep track of bounce number
+    bounce_number += 1
+    # take the height and multiply by ratio
+    height = height * ratio
+
+    print(bounce_number, round(height, 4))
+
+
+
